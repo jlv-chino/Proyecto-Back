cb15a0d6fa763e269390f386778bec0157814d61
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-04-20 15:12:48 -0300

    Para subir...

--- src/main/java/com/portfolio/JoseLuisVilches/controlador/PersonaControlador.java
+++ src/main/java/com/portfolio/JoseLuisVilches/controlador/PersonaControlador.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/persona")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaControlador {
 
     @Autowired
