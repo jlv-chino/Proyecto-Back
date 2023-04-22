cdd67be741d1c906da9d108a4946e4461147c08b
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-04-21 21:40:02 -0300

    Commit para pasar a rama Skill

---
+++ src/main/java/com/portfolio/JoseLuisVilches/controlador/SkillControlador.java
@@ -0,0 +1,13 @@
+
+package com.portfolio.JoseLuisVilches.controlador;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/skill")
+@CrossOrigin(origins = "http://localhost:4200")
+public class SkillControlador {
+    
+}
