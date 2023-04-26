04f4d5e25c0b9cca1a8584eafd8f09e9944915dd
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-04-26 14:51:04 -0300

    Cambiamos tipo de dato a string en inicio_experiencia

--- src/main/java/com/portfolio/JoseLuisVilches/modelo/Experiencia.java
+++ src/main/java/com/portfolio/JoseLuisVilches/modelo/Experiencia.java
@@ -23,14 +23,14 @@ public class Experiencia {
     String compania_experiencia;
     String modalidad_experiencia;
     String imagen_experiencia;
-    Date inicio_experiencia;
+    String inicio_experiencia;
     String fin_experiencia;
     Date tiempo_experiencia;
 
     public Experiencia() {
     }
 
-    public Experiencia(long id, String puesto_experiencia, String compania_experiencia, String modalidad_experiencia, String imagen_experiencia, Date inicio_experiencia, String fin_experiencia, Date tiempo_experiencia) {
+    public Experiencia(long id, String puesto_experiencia, String compania_experiencia, String modalidad_experiencia, String imagen_experiencia, String inicio_experiencia, String fin_experiencia, Date tiempo_experiencia) {
         this.id = id;
         this.puesto_experiencia = puesto_experiencia;
         this.compania_experiencia = compania_experiencia;
