9a7cb8ac73d2cf9a7b5156afc4d3f43520fceb96
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-04-30 22:19:51 -0300

    Error de webSecusity

---
+++ src/main/java/com/portfolio/JoseLuisVilches/Security/Dto/LoginUsuario.java
@@ -0,0 +1,30 @@
+
+package com.portfolio.JoseLuisVilches.Security.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+
+public class LoginUsuario {
+    
+    @NotBlank
+    private String nombreUsuario;
+    @NotBlank
+    private String password;
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
--- src/main/java/com/portfolio/JoseLuisVilches/Security/Service/UsuarioService.java
+++ src/main/java/com/portfolio/JoseLuisVilches/Security/Service/UsuarioService.java
@@ -22,7 +22,7 @@ public class UsuarioService {
         return iusuarioRepository.existsByNombreUsuario(nombreUsuario);
     }
     
-    public boolean existsByemail(String email){
+    public boolean existsByEmail(String email){
         return iusuarioRepository.existsByEmail(email);
     }
     
